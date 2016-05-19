896a552eae3143017ab1fa2d4e7c1df81e533027
Jack Franklin <jack@jackfranklin.net>
2016-05-19 12:13:31 +0200

    26 and 27

---
+++ values/26.js
@@ -0,0 +1,51 @@
+var getGitHubUser = function(name) {
+  return fetch('https://api.github.com/users/' + name).then(function(data) {
+    return data.json();
+  });
+};
+
+var Common = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Link to="/" activeClassName="active-link">INDEX</Link>
+        { this.props.children }
+      </div>
+    );
+  }
+});
+
+var Index = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+  go: function() {
+    this.context.router.push('/github/jackfranklin');
+  },
+  render: function() {
+    return (
+      <div>
+        <h1>Index page!</h1>
+        <button onClick={this.go}>View Jack's GitHub data</button>
+      </div>
+    );
+  }
+});
+
+var GitHubPerson = React.createClass({
+  // do your work here!
+  render: function() {
+    return <p>Your work here</p>;
+  }
+});
+
+var App = function() {
+  return (
+    <Router history={hashHistory}>
+      <Route path="" component={Common}>
+        <Route path="/" component={Index} />
+        // define your new route here
+      </Route>
+    </Router>
+  );
+}
---
+++ values/27.js
@@ -0,0 +1,68 @@
+var getGitHubUser = function(name) {
+  return fetch('https://api.github.com/users/' + name).then(function(data) {
+    return data.json();
+  });
+};
+
+var Common = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Link to="/" activeClassName="active-link">INDEX</Link> | <Link to="/github" activeClassName="active-link">GitHub Index</Link>
+        { this.props.children }
+      </div>
+    );
+  }
+});
+
+var Index = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+  go: function() {
+    this.context.router.push('/github/jackfranklin');
+  },
+  render: function() {
+    return (
+      <div>
+        <h1>Index page!</h1>
+        <button onClick={this.go}>View Jack's GitHub data</button>
+      </div>
+    );
+  }
+});
+
+var GitHubPerson = React.createClass({
+  getInitialState() {
+    return { user: {} };
+  },
+  componentWillMount() {
+    getGitHubUser(this.props.params.name).then(function(data) {
+      this.setState({ user: data });
+    }.bind(this));
+  },
+  render: function() {
+    return <p>Name: { this.state.user.name } from { this.state.user.company }</p>;
+  }
+});
+
+var GitHubIndex = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+  render: function() {
+    return <p>Create a text box and button here.</p>
+  }
+});
+
+var App = function() {
+  return (
+    <Router history={hashHistory}>
+      <Route path="" component={Common}>
+        <Route path="/" component={Index} />
+        <Route path="/github" component={GitHubIndex} />
+        <Route path="/github/:name" component={GitHubPerson} />
+      </Route>
+    </Router>
+  );
+}
